b483d0074a7186da4f535020181796096b085a24
苏征辉 <343196323@qq.com>
2024-06-25 23:41:51 +0800

    ✨ feat: 新增客户端运行组件

---
+++ src/runtime/client-entry.tsx
@@ -0,0 +1,13 @@
+import { createRoot } from "react-dom/client"
+import { App } from "./App"
+
+function renderInBrower() {
+	// 根元素
+	const containerEl = document.getElementById("root")
+	if (!containerEl) {
+		throw new Error("#root element not found")
+	}
+	createRoot(containerEl).render(<App />)
+}
+
+renderInBrower()
