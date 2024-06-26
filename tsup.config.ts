387ab6f022481a536f54819f2eed450700a06b2a
苏征辉 <343196323@qq.com>
2024-06-26 17:42:56 +0800

    🔧 build: 修改ts和tsup配置

---
+++ tsup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "tsup"
+
+export default defineConfig({
+	entry: ["src/node/cli.ts"],
+	bundle: true,
+	splitting: true,
+	minify: process.env.NODE_ENV === "production",
+	outDir: "dist",
+	format: ["cjs", "esm"],
+	dts: true,
+	// 开启shims解决一些API的兼容比如__dirname
+	shims: true,
+})
