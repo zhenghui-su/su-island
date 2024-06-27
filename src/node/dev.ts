5f8e07d5aff2a31a6aee410817b76210a589d025
苏征辉 <343196323@qq.com>
2024-06-27 21:28:26 +0800

    🧪 test: 测试开发服务器是否能解析文件

--- src/node/dev.ts
+++ src/node/dev.ts
@@ -2,6 +2,7 @@ import { createServer as createViteDevServer } from 'vite';
 import { pluginIndexHtml } from '../plugin-island/indexHtml';
 import react from '@vitejs/plugin-react';
 import { PACKAGE_ROOT } from './constants';
+import { resolveConfig } from './config';
 /**
  * 用于创建开发服务器
  *
@@ -9,6 +10,8 @@ import { PACKAGE_ROOT } from './constants';
  * @returns 开发服务器
  */
 export async function createDevServer(root: string) {
+  const config = await resolveConfig(root, 'serve', 'development');
+  console.log(config);
   return createViteDevServer({
     root,
     plugins: [pluginIndexHtml(), react()],
