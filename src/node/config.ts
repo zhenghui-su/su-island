194e2a90bb651eb790fa951dc9b701d63635922b
苏征辉 <343196323@qq.com>
2024-06-27 21:27:09 +0800

    ✨ feat: 新增解析用户配置文件功能

---
+++ src/node/config.ts
@@ -0,0 +1,66 @@
+import { resolve } from 'path';
+import fs from 'fs-extra';
+import { loadConfigFromFile } from 'vite';
+import { UserConfig } from '../shared/types/index';
+
+type RawConfig =
+  | UserConfig
+  | Promise<UserConfig>
+  | (() => UserConfig | Promise<UserConfig>);
+
+/**
+ * 用于获取用户配置文件路径
+ * @param root 根目录
+ */
+function getUserConfigPath(root: string) {
+  try {
+    // 支持的配置文件格式
+    const supportConfigFiles = ['config.ts', 'config.js'];
+    // 找到存在的配置文件路径并返回
+    const configPath = supportConfigFiles
+      .map((file) => resolve(root, file))
+      .find(fs.pathExistsSync);
+    return configPath;
+  } catch (e) {
+    console.log('Failed to load user config.');
+    throw e;
+  }
+}
+
+/**
+ * 用于解析用户配置文件
+ *
+ * @param root 根目录
+ * @param command 'serve' | 'build'
+ * @param mode 'production' | 'development'
+ */
+export async function resolveConfig(
+  root: string,
+  command: 'serve' | 'build',
+  mode: 'production' | 'development'
+) {
+  // 1. 获取配置文件路径, 支持 js、ts 格式
+  const configPath = getUserConfigPath(root);
+  // 2. 解析配置文件
+  const result = await loadConfigFromFile(
+    {
+      command,
+      mode
+    },
+    configPath,
+    root
+  );
+  if (result) {
+    const { config: rawConfig = {} as RawConfig } = result;
+    // 三种形式
+    // 1. object
+    // 2. promise
+    // 3. function 返回 object或promise
+    const userConfig = await (typeof rawConfig === 'function'
+      ? rawConfig()
+      : rawConfig);
+    return [configPath, userConfig] as const;
+  } else {
+    return [configPath, {} as UserConfig] as const;
+  }
+}
