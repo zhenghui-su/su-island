92e211dd800957b55a89be3d1d48dbb91fdae813
苏征辉 <343196323@qq.com>
2024-06-28 23:36:37 +0800

    ✨ feat: 使用自动生成的约定式路由

--- src/runtime/Content.tsx
+++ src/runtime/Content.tsx
@@ -1,25 +1,5 @@
-import A from '../../docs/guide/a';
-import B from '../../docs/b';
-import Index from '../../docs/guide/index';
 import { useRoutes } from 'react-router-dom';
-
-/**
- * 路由配置-手动版本
- */
-const routes = [
-  {
-    path: '/guide',
-    element: <Index />
-  },
-  {
-    path: '/guide/a',
-    element: <A />
-  },
-  {
-    path: '/b',
-    element: <B />
-  }
-];
+import { routes } from 'su-island:routes';
 
 export const Content = () => {
   const routeElement = useRoutes(routes);
