c068760c9a47cc2dfa0fe694f2b12d10cb5c02e7
苏征辉 <343196323@qq.com>
2024-06-30 16:35:58 +0800

    🧪 test: 测试编译md大代码块结果

--- src/node/__test__/md.test.ts
+++ src/node/__test__/md.test.ts
@@ -3,12 +3,17 @@ import remarkParse from 'remark-parse';
 import remarkRehype from 'remark-rehype';
 import rehypeStringify from 'rehype-stringify';
 import { describe, test, expect } from 'vitest';
+import { rehypePluginPreWrapper } from '../../node/plugin-mdx/rehypePlugins/preWrapper';
 
 // markdown语法编译测试
 describe('Markdown complie cases', async () => {
   const processor = unified();
 
-  processor.use(remarkParse).use(remarkRehype).use(rehypeStringify);
+  processor
+    .use(remarkParse)
+    .use(remarkRehype)
+    .use(rehypeStringify)
+    .use(rehypePluginPreWrapper);
   // 测试编译标题
   test('Compile title', async () => {
     const mdTitle = '# 123';
@@ -22,5 +27,19 @@ describe('Markdown complie cases', async () => {
     expect(result.value).toMatchInlineSnapshot(
       '"<p>I am using <code>su-island.js</code></p>"'
     );
+  });
+  // 测试编译大代码块
+  test('Compile code block', async () => {
+    const mdCodeBlock = '```js\nconsole.log(123);\n```';
+    const result = processor.processSync(mdCodeBlock);
+    // 结构应该如下
+    // <div class="language-js">
+    //   <span class="lang">js</span>
+    //   <pre><code>...</code></pre>
+    // </div>
+    expect(result.value).toMatchInlineSnapshot(`
+      "<div class=\\"language-js\\"><span class=\\"lang\\">js</span><pre><code class=\\"\\">console.log(123);
+      </code></pre></div>"
+    `);
   });
 });
