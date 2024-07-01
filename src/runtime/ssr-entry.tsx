fb32da047ea94b6c2d6ba83ba9547c2efccecfc3
suzhenghui <343196323@qq.com>
2024-07-01 13:06:10 +0800

    ✨ feat: 新增生产环境多路由的打包

--- src/runtime/ssr-entry.tsx
+++ src/runtime/ssr-entry.tsx
@@ -4,11 +4,16 @@ import { StaticRouter } from 'react-router-dom/server';
 
 /**
  * 用于服务端渲染-拿到组件的html字符串
+ * @param 路由传参
  */
-export function render() {
+export function render(pagePath: string) {
   return renderToString(
-    <StaticRouter location={'/guide/a'}>
+    <StaticRouter location={pagePath}>
       <App />
     </StaticRouter>
   );
 }
+/**
+ * 导出路由数据
+ */
+export { routes } from 'su-island:routes';
