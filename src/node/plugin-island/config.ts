6cd79e73865ffe47b9f5625b34e45c353ac33dcd
苏征辉 <343196323@qq.com>
2024-07-03 21:03:03 +0800

    🎈 perf: css模块驼峰规范和添加sass类型模块

--- src/node/plugin-island/config.ts
+++ src/node/plugin-island/config.ts
@@ -36,6 +36,11 @@ export function pluginConfig(
           alias: {
             '@runtime': join(PACKAGE_ROOT, 'src', 'runtime', 'index.ts')
           }
+        },
+        css: {
+          modules: {
+            localsConvention: 'camelCaseOnly'
+          }
         }
       };
     },
