2aac62dedefe66008929e1153b8d900ecc01a001
苏征辉 <343196323@qq.com>
2024-07-03 22:17:45 +0800

    ✨ feat: 新增Button公共组件

---
+++ src/theme-default/components/Button/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link } from '../Link';
+import styles from './index.module.scss';
+
+interface ButtonProps {
+  type?: string;
+  size?: 'medium' | 'big';
+  theme?: 'brand' | 'alt';
+  text: string;
+  href?: string;
+  external?: boolean;
+  className?: string;
+}
+/**
+ * Button 组件
+ */
+export function Button(props: ButtonProps) {
+  const {
+    theme = 'brand',
+    size = 'big',
+    href = '/',
+    external = false,
+    className = ''
+  } = props;
+  let type: string | typeof Link | null = null;
+
+  if (props.type === 'button') {
+    type = 'button';
+  } else if (props.type === 'a') {
+    type = external ? 'a' : Link;
+  }
+
+  return React.createElement(
+    type ?? 'a',
+    {
+      className: `${styles.button} ${styles[theme]} ${styles[size]} ${className}`,
+      href
+    },
+    props.text
+  );
+}
