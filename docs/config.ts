20d99bb0fa10597deb551d0cbb2919d6cdf59504
苏征辉 <343196323@qq.com>
2024-07-03 21:06:08 +0800

    🧪 test: 编写配置测试导航栏生成

--- docs/config.ts
+++ docs/config.ts
@@ -2,5 +2,17 @@ import { defineConfig } from '../dist';
 
 export default defineConfig({
   title: 'su-island',
-  description: 'su-island'
+  description: 'su-island',
+  themeConfig: {
+    nav: [
+      {
+        text: '主页',
+        link: '/'
+      },
+      {
+        text: '指南',
+        link: '/'
+      }
+    ]
+  }
 })
