f8c146ef5253aa16d17a5d7996a0a9b3a4e4b73f
suzhenghui <343196323@qq.com>
2024-07-04 16:53:49 +0800

    🧪 test: 测试侧边栏生成

--- docs/config.ts
+++ docs/config.ts
@@ -2,17 +2,28 @@ import { defineConfig } from '../dist';
 
 export default defineConfig({
   title: 'su-island',
-  description: 'su-island',
   themeConfig: {
     nav: [
-      {
-        text: '主页',
-        link: '/'
-      },
-      {
-        text: '指南',
-        link: '/'
-      }
-    ]
+      { text: '主页', link: '/' },
+      { text: '指南', link: '/guide/' }
+    ],
+    // 新增 sidebar 的内容
+    sidebar: {
+      '/guide/': [
+        {
+          text: '教程',
+          items: [
+            {
+              text: '快速上手',
+              link: '/guide/a'
+            },
+            {
+              text: '如何安装',
+              link: '/guide/b'
+            }
+          ]
+        }
+      ]
+    }
   }
-})
+});
